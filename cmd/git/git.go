eecabedc0399edff1d452da0fd6c84f285987d52
linonymous <iamlinonymous@gmail.com>
2021-12-21 19:36:33 +0530

    DA-4560 - add new publisher - update UserIdentity to UserIdentityObjectBase
    Signed-off-by: linonymous <iamlinonymous@gmail.com>

--- cmd/git/git.go
+++ cmd/git/git.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"github.com/LF-Engineering/lfx-event-schema/utils/datalake"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"io"
 	"math"
 	"net/url"
@@ -15,8 +18,6 @@ import (
 	"strings"
 	"sync"
 	"time"
-
-	"github.com/LF-Engineering/insights-datasource-shared/firehose"
 
 	"github.com/LF-Engineering/lfx-event-schema/service/user"
 
@@ -66,6 +67,8 @@ const (
 	GitGenerateFlatDocs = true
 	// GitDefaultStream - Stream To Publish
 	GitDefaultStream = "PUT-S3-git-commits"
+	// GitDataSource - constant for git source
+	GitDataSource = "git"
 )
 
 var (
@@ -490,7 +493,7 @@ var (
 
 // Publisher - for streaming data to Kinesis
 type Publisher interface {
-	PutRecordBatch(channel string, records []interface{}) ([]*firehose.PutResponse, error)
+	PushEvents(source, eventType, subEventType string, data []interface{}) error
 }
 
 // RawPLS - programming language summary (all fields as strings)
@@ -657,8 +660,14 @@ func (j *DSGit) Init(ctx *shared.Ctx) (err error) {
 		shared.Printf("git: %+v\nshared context: %s\nModel: %+v", j, ctx.Info(), m)
 	}
 	if j.Stream != "" {
-		kinesisClient, _ := firehose.NewClientProvider()
-		j.AddPublisher(kinesisClient)
+		sess, err := session.NewSession()
+		if err != nil {
+			return err
+		}
+		s3Client := s3.New(sess)
+		objectStore := datalake.NewS3ObjectStore(s3Client)
+		datalakeClient := datalake.NewStoreClient(&objectStore)
+		j.AddPublisher(&datalakeClient)
 	}
 	return
 }
@@ -1084,7 +1093,7 @@ func (j *DSGit) GetModelData(ctx *shared.Ctx, docs []interface{}) []insights.Com
 				username := ""
 				email := ident[2]
 				name, username = shared.PostprocessNameUsername(name, username, email)
-				commitRole.Identity = user.UserIdentity{
+				commitRole.Identity = user.UserIdentityObjectBase{
 					Email:      email,
 					FirstName:  name,
 					IsVerified: false,
@@ -1507,7 +1516,7 @@ func (j *DSGit) GitEnrichItems(ctx *shared.Ctx, thrN int, items []interface{}, d
 				for _, d := range data {
 					formattedData = append(formattedData, d)
 				}
-				_, err := j.Publisher.PutRecordBatch(j.Stream, formattedData)
+				err := j.Publisher.PushEvents("insights", GitDataSource, "commits", formattedData)
 				if err != nil {
 					shared.Printf("Error: %+v\n", err)
 				}
--- go.mod
+++ go.mod
@@ -2,13 +2,16 @@ module github.com/LF-Engineering/insights-datasource-git
 
 go 1.15
 
+replace github.com/LF-Engineering/lfx-event-schema => /Users/swapnilwalke/Documents/go-workspace/LF-Engineering/lfx-event-schema
+
 require (
 	github.com/LF-Engineering/insights-datasource-shared v1.1.2
-	github.com/LF-Engineering/lfx-event-schema v0.0.0-20211209223404-ff77e00da5bd
-	github.com/go-openapi/errors v0.20.1
-	github.com/go-openapi/strfmt v0.21.1
-	github.com/go-openapi/swag v0.19.15
-	github.com/go-openapi/validate v0.20.3
+	github.com/LF-Engineering/lfx-event-schema v0.0.0-20211216071810-45fc9f544c82
+	github.com/aws/aws-lambda-go v1.27.1 // indirect
 	github.com/json-iterator/go v1.1.11
+	github.com/kr/text v0.2.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
