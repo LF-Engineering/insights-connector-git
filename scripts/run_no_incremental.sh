6c01e05a31e3f34b5385781892e370e9651f3b71
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-02-17 08:31:24 +0000

    Fix DA-4739
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- scripts/run_no_incremental.sh
+++ scripts/run_no_incremental.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-GIT_NO_INCREMENTAL=1 ./scripts/git.sh --git-stream='' --git-url='http://dpdk.org/git/dpdk-stable' > out
+GIT_NO_INCREMENTAL=1 ./scripts/git.sh --git-stream='' --git-url='https://gerrit.onap.org/r/logging-analytics' > out
