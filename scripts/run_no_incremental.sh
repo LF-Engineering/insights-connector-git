e7769454b5305e97ed8c38ceac9a53d06598e0d8
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-02-23 06:40:40 +0000

    Update the run script
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- scripts/run_no_incremental.sh
+++ scripts/run_no_incremental.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-GIT_NO_INCREMENTAL=1 ./scripts/git.sh --git-stream='' --git-url='https://github.com/cloudfoundry/loggregator' > out
+GIT_NO_INCREMENTAL=1 ./scripts/git.sh --git-stream='' --git-url='https://github.com/magma/magma-website' 2>&1 | tee -a run.log
