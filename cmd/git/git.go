28b551c60845bc3377ae97679c978ac5d1edc050
Ayman <enkhalifapro@gmail.com>
2022-12-16 22:05:14 +0200

    format date
    Signed-off-by: Ayman <enkhalifapro@gmail.com>

--- cmd/git/git.go
+++ cmd/git/git.go
@@ -943,7 +943,7 @@ func (j *DSGit) EnrichItem(ctx *shared.Ctx, item map[string]interface{}) (rich m
 		err = fmt.Errorf("cannot parse author date from %v", iAuthorDate)
 		return
 	}
-	authorLocalTime, err := shared.TimeParseAny(strings.TrimSpace(sAuthorDate))
+	authorLocalTime, err := time.Parse(time.RFC3339, strings.TrimSpace(sAuthorDate))
 	if !ok {
 		err = fmt.Errorf("cannot parse commit local date from %v", sAuthorDate)
 		return
@@ -964,7 +964,7 @@ func (j *DSGit) EnrichItem(ctx *shared.Ctx, item map[string]interface{}) (rich m
 		err = fmt.Errorf("cannot parse commit date from %v", iCommitDate)
 		return
 	}
-	commitLocalTime, err := shared.TimeParseAny(strings.TrimSpace(sCommitDate))
+	commitLocalTime, err := time.Parse("2006-01-02T15:04:05-0700", strings.TrimSpace(sCommitDate))
 	if !ok {
 		err = fmt.Errorf("cannot parse commit local date from %v", sCommitDate)
 		return
