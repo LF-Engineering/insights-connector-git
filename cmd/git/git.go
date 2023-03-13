d5e419abebed1c8f3ca29c150cce67b9fa064061
khalifapro <81648906+khalifapro@users.noreply.github.com>
2023-03-13 16:53:19 +0200

    decrease pagging period to a month (#90)
    Signed-off-by: Ayman <enkhalifapro@gmail.com>
    Co-authored-by: Ayman <enkhalifapro@gmail.com>

--- cmd/git/git.go
+++ cmd/git/git.go
@@ -3134,7 +3134,7 @@ func (j *DSGit) SyncV2(ctx *shared.Ctx) (err error) {
 	}
 
 	for from.Before(headCommit.Author.When) {
-		until := from.Add(24 * time.Hour * 90)
+		until := from.Add(24 * time.Hour * 30)
 		comms, er := getRepoCommits(r, from, until)
 		if er != nil {
 			err = er
